b23c8074a5163dfb47a59ac1b11beea76591f4d0
Raza Anees <164820623+Raza-Anees@users.noreply.github.com>
2024-11-27 17:38:52 +0500

    new

--- dbConfig.ts
+++ dbConfig.ts
@@ -2,7 +2,7 @@ import { Property } from 'src/entities/property.entity';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export const pgConfig: PostgresConnectionOptions = {
-  url: 'postgresql://realEstateDB_owner:bRGx5hj8BQtE@ep-wild-bird-a5g3v35j.us-east-2.aws.neon.tech/realEstateDB?sslmode=require',
+  url: '',
   type: 'postgres',
   port: 3306,
   entities: [Property],
